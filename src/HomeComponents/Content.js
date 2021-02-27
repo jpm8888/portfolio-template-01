46830dcf650f228bb5e0f88608996cb2e4b489d6
jpm <jpm8888@gmail.com>
2021-02-27 10:41:59 +0530

    fixed content, made components from raw code

--- src/HomeComponents/Content.js
+++ src/HomeComponents/Content.js
@@ -1,39 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import image2 from "../assets/home/img2.jpg";
 import image6 from '../assets/home/img5.jpg';
 import image7 from '../assets/home/img6.jpg';
-import image5 from "../assets/home/right.svg";
 import './css/Content.css';
 
 class Content extends Component {
+
     render() {
         return (
             <div>
-                <div className="container ml-5" id="container">
+                <div className="container ml-5 container_margin">
                     <div className="row">
-                        <div className="col-md-4" id="div1">
-                            <img src={image2} className="img-fluid"></img> <label className="figure-caption mt-4">Designed associated with creativity and grapgics</label>
-                            <div id="ui-ux"><h4 className="mt-4 figure-caption font-weight-bolder text-dark">UI-UX Design <img src={image5} height='30px' width='30px'></img></h4></div>
-                        </div>
-                        <div className="col-md-4" id="div2">
-                            <img src={image6} className="img-fluid"></img> <label className="figure-caption mt-4">Designed associated with creativity and grapgics</label>
-                            <div id="Branding"><h4 className="mt-4 figure-caption font-weight-bolder text-dark">Branding Idea <img src={image5} height='30px' width='30px'></img></h4></div>
-                        </div>
-                        <div className="col-md-4" id="div3">
-                            <img src={image7} className="img-fluid"></img> <label className="figure-caption mt-4">Designed associated with creativity and grapgics</label>
-                            <div id="Natural"><h4 className="mt-4 figure-caption font-weight-bolder text-dark">Natural Food <img src={image5} height='30px' width='30px'></img></h4></div>
-                        </div>
+                        <ImageContainer caption={'Designed associated with creativity and graphics1'} image={image2} bottomText={'Hello1'} />
+                        <ImageContainer caption={'Designed associated with creativity and graphics2'} image={image6} bottomText={'Hello2'} />
+                        <ImageContainer caption={'Designed associated with creativity and graphics3'} image={image7} bottomText={'Hello3'} />
                     </div>
-
-
-
-
                 </div>
             </div>
 
         );
     }
 };
+
+const ImageContainer = (props) => {
+    let { caption, image, bottomText } = props;
+
+    return (
+        <div className="col-md-4">
+            <div className={'image_container'}>
+                <img src={image} className="img-fluid" />
+                <div className="bottom_left_div">
+                    {bottomText}
+                </div>
+            </div>
+            <label className="figure-caption mt-4">{caption}</label>
+
+            {/* <div id="ui-ux">
+                <h4 className="mt-4 figure-caption font-weight-bolder text-dark">UI-UX Design<img src={image} height='30px' width='30px'></img></h4>
+            </div> */}
+        </div>
+    )
+}
 
 
 export default Content;
--- src/HomeComponents/UpNavBar.js
+++ src/HomeComponents/UpNavBar.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './css/UpNavBar.css';
 
 class UpNavBar extends Component {
@@ -7,16 +7,24 @@ class UpNavBar extends Component {
             <div>
                 <nav className="navbar  navbar-expand-md navbar-light bg-light align-center ml-5">
                     <ul className="navbar-nav" id="navbar-nav">
-                        <li className="nav-item"><a href="#" className="nav-link">selected work</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">branding</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">digital experience</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">graphics design</a></li>
-                        <li className="nav-item"><a href="#" className="nav-link">custom printing</a></li>
+                        <LiItem name={'selected work'} href={'#'} />
+                        <LiItem name={'branding'} href={'#'} />
+                        <LiItem name={'digital experience'} href={'#'} />
+                        <LiItem name={'graphic design'} href={'#'} />
+                        <LiItem name={'custom printing'} href={'#'} />
                     </ul>
                 </nav>
             </div>
         );
     }
+}
+
+const LiItem = (props) => {
+    let { name, href } = props;
+
+    return (
+        <li className="nav-item"><a href={href} className="nav-link">{name}</a></li>
+    )
 }
 
 export default UpNavBar;
