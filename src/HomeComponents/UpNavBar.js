1908dabcacffc26863683b6732c4a53a90405e2d
whiteFang <jpm8888@gmail.com>
2021-03-12 10:29:26 +0530

    minor fixes

--- src/HomeComponents/UpNavBar.js
+++ src/HomeComponents/UpNavBar.js
@@ -10,31 +10,27 @@ export default class Menu extends Component {
     this.toggleMenu = this.toggleMenu.bind(this);
   }
 
-  toggleMenu(){
+  toggleMenu() {
     this.setState({ menu: !this.state.menu })
   }
 
   render() {
-
-  const show = (this.state.menu) ? "show" : "" ;
-
-  return (
-
-    <nav className="navbar navbar-expand-lg navbar-light ml-5" >
-      <button className="navbar-toggler" type="button" onClick={ this.toggleMenu } id="button">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className={"collapse navbar-collapse " + show}>
-        <div className="navbar-nav" id="navbar-nav">
-         <a className="nav-item nav-link" href="#">selected work</a>
-          <a className="nav-item nav-link" href="#">branding</a>
-          <a className="nav-item nav-link" href="#">digital experiences</a>
-          <a className="nav-item nav-link" href="#">graphics design</a>
-          <a className="nav-item nav-link" href="#">custom printing</a>
+    const show = (this.state.menu) ? "show" : "";
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light ml-5" >
+        <button className="navbar-toggler" type="button" onClick={this.toggleMenu} id="button">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={"collapse navbar-collapse " + show}>
+          <div className="navbar-nav" id="navbar-nav">
+            <a className="nav-item nav-link" href="#">selected work</a>
+            <a className="nav-item nav-link" href="#">branding</a>
+            <a className="nav-item nav-link" href="#">digital experiences</a>
+            <a className="nav-item nav-link" href="#">graphics design</a>
+            <a className="nav-item nav-link" href="#">custom printing</a>
+          </div>
         </div>
-      </div>
-    </nav>
-
-  );
+      </nav>
+    );
   }
 }
